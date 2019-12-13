a028adf59d2455bcba9b7e06ecb17cc850342500
Jadidiah-Harris <emiya59410@gmail.com>
2019-12-14 01:02:50 +0800

    Site updated: 2019-12-14 01:02:40

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,15 @@
+
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
+    container:document.getElementById('aplayer'),
     fixed: true,
     autoplay: true,
     audio: [
       {
-        name: '魔法使いの夜～メインテーマ',
-        artist: 'Hideyuki Fukasawa',
-        url: 'http://music.163.com/song/media/outer/url?id=501638.mp3',
-        cover: '',
-      }，
-      {
-        name: '運命の夜',
-        artist: '川井憲次 / 佐々木寿子',
-        url: 'http://music.163.com/song/media/outer/url?id=448060.mp3',
-        cover: '',
-      }，
-      {
-        name: '騎士王の誇り',
-        artist: '川井憲次',
-        url: 'http://music.163.com/song/media/outer/url?id=448119.mp3',
-        cover: '',
+        name: "暗涌",
+        artist: '王菲',
+        url: 'source\music魔法使いの夜～メインテーマ.mp3',
+        cover: 'http://p1.music.126.net/w8RFsMH8VJfPsBmVudYGsA==/109951163020569833.jpg?param=130y130',
       }
-    ]
+     
 });
+
